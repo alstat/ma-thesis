124949cb2a8c670ee46af7ec1141e2e5689eb50a
alstat <alahmadgaid@gmail.com>
2025-04-06 21:12:31 +0800

    delete

--- codes/descriptive/morphological_analysis.jl
+++
@@ -1,9 +0,0 @@
-using QuranTree
-using Makie
-using CairoMakie
-using MakieThemes
-
-crps, tnzl = load(QuranData());
-crpstbl = table(crps)
-tnzltbl = table(tnzl)
-
--- codes/julia/config.jl
+++
@@ -1,3 +0,0 @@
-using PromptingTools
-ENV["OPENAI_API_KEY"] = "sk-proj-1Ym3U1M20cVY90xljwnJT3BlbkFJnheiXg9PJZWHXijiJYMt"
-PromptingTools.OPENAI_API_KEY="sk-proj-1Ym3U1M20cVY90xljwnJT3BlbkFJnheiXg9PJZWHXijiJYMt"
